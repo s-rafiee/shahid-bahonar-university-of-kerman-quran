8fe145ef92109958a5c7065c5f9d46b70c817dd3
Saman Rafiee <saman.rafiee.rs@gmail.com>
2022-03-01 00:27:18 +0330

    feat(apps): create and link urls.py

--- customLogin/urls.py
+++ customLogin/urls.py
@@ -13,13 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from django.urls import path
 
 urlpatterns = [
-                  path('', include('home.urls')),
-                  path('', include('dashboard.urls')),
-                  path('', include('activities.urls')),
-                  path('', include('blog.urls')),
-              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+]
