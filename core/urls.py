150970bd09c0ded2fcd081cd24615d456bf78730
Saman Rafiee <saman.rafiee.rs@gmail.com>
2022-03-01 13:28:04 +0330

    fix(login-setting): fix login error

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,7 @@
 """core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+                  path('admin/', admin.site.urls),
                   path('', include('home.urls')),
                   path('', include('dashboard.urls')),
                   path('', include('activities.urls')),
